cdaf66a0b4bdf3277e21c9ba0e0f093971ba1399
Samara Souza <samyesouza13@gmail.com>
2025-01-16 16:50:47 -0300

    new cards for posts

--- frontend/src/components/postsComponents/PostList.jsx
+++ frontend/src/components/postsComponents/PostList.jsx
@@ -1,60 +1,71 @@
 import {
   Box,
   Button,
+  Center,
   Stack,
   Text,
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
+  Heading,
 } from "@chakra-ui/react";
 import PropTypes from "prop-types";
-import { useContext } from 'react';
-import { AuthContext } from "../../AppContext.jsx"
+import { useContext } from "react";
+import { AuthContext } from "../../AppContext.jsx";
 
 const PostList = ({ posts, onEdit, onDelete }) => {
-    const { userId } = useContext(AuthContext)
-    return (
+  const { userId } = useContext(AuthContext);
+  return (
     <Stack spacing={4} width={"100%"} p={4} bg={"#F8EDEB"} borderRadius={"lg"}>
       {posts && posts.length > 0 ? (
         posts.map((post) => (
-          <Accordion key={post.id} allowToggle>
-            <AccordionItem>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">
-                  <Text fontSize="xl">{post.title}</Text>
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-              <AccordionPanel pb={4}>
-                <Text fontSize="md">{post.content}</Text>
-                <Text fontSize="sm" color="gray.500">
-                  Posted by: {post.author.name}
+          <Center py={6} key={post.id}>
+            <Box
+              maxW={"445px"}
+              w={"full"}
+              boxShadow={"2xl"}
+              rounded={"md"}
+              p={6}
+              overflow={"hidden"}
+            >
+              <Stack>
+                <Text
+                  color={"green.500"}
+                  textTransform={"uppercase"}
+                  fontWeight={800}
+                  fontSize={"sm"}
+                  letterSpacing={1.1}
+                >
+                  {post.author.name}
                 </Text>
-                <Text fontSize="sm" color="gray.500">
-                  Created at: {new Date(post.createdAt).toLocaleString()}
-                </Text>
-                <Text fontSize="sm" color="gray.500">
-                  Updated at: {new Date(post.updatedAt).toLocaleString()}
-                </Text>
-                {post.authorid === userId && (
-                  <Box mt={4}>
-                    <Button colorScheme="blue" onClick={() => onEdit(post)}>
-                      Edit
-                    </Button>
-                    <Button
-                      colorScheme="red"
-                      onClick={() => onDelete(post.id)}
-                      ml={2}
-                    >
-                      Delete
-                    </Button>
-                  </Box>
-                )}
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
+                <Heading fontSize={"2xl"} fontFamily={"body"}>
+                  {post.title}
+                </Heading>
+                <Text color={"gray.500"}>{post.content}</Text>
+              </Stack>
+              <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
+                <Stack direction={"column"} spacing={0} fontSize={"sm"}>
+                  <Text fontWeight={600}>
+                    Created at: {new Date(post.createdAt).toLocaleString()}
+                  </Text>
+                  <Text color={"gray.500"}>
+                    Updated at: {new Date(post.updatedAt).toLocaleString()}
+                  </Text>
+                  {post.authorid === userId && (
+                    <Box mt={4}>
+                      <Button colorScheme="blue" onClick={() => onEdit(post)}>
+                        Edit
+                      </Button>
+                      <Button
+                        colorScheme="red"
+                        onClick={() => onDelete(post.id)}
+                        ml={2}
+                      >
+                        Delete
+                      </Button>
+                    </Box>
+                  )}
+                </Stack>
+              </Stack>
+            </Box>
+          </Center>
         ))
       ) : (
         <Text>No posts available</Text>
